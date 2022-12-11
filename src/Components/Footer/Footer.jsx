86eecc53a4dff04ba7ce1b8243de94f41d806513
pasha <pashabezk@mail.ru>
2022-12-11 16:20:32 +0300

    change footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css"
 const AppFooter = () => {
 	return (
 		<div className={styles.mainFooter}>
-			<p>Created by <a href="https://pashabezk.github.io/MainPage" target="_blank" rel="noreferrer">pashabezk</a></p>
+			<p>Created by <a href="https://pashabezk.github.io/MainPage" target="_blank" rel="noreferrer">pashabezk</a> & <a href="https://github.com/madpenguinw" target="_blank" rel="noreferrer">Михаил</a></p>
 		</div>
 	);
 }
