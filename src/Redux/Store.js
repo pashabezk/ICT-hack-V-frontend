4236ac6f089618704def54100d9082e91cd3952a
pasha <pashabezk@mail.ru>
2022-12-11 16:07:38 +0300

    try add profile API support2

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,11 +1,13 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import authReducer from "./AuthReducer"
 import projectsReducer from "./ProjectsReducer"
+import profileReducer from "./ProfileReducer"
 
 const store = configureStore({
 	reducer: combineReducers({
 		auth: authReducer,
 		projects: projectsReducer,
+		profile: profileReducer,
 		// alfaBank: alfaBankReducer
 	})
 });
